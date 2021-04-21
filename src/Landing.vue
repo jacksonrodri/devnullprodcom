312c9ae251db489eef77a9be521bcb0d65c97d0b
jacksonrodri <jacksonrodriguez329@gmail.com>
2021-04-21 08:39:03 -0400

    Adjust for SCSS and working on Landing page

--- src/Landing.vue
+++ src/Landing.vue
@@ -5,16 +5,72 @@
         <h1>Engineering Solutions for The Digital Age</h1>
       </div>
 
-      <div id="content">
-        <p>Dev Null Productions was founded in 2018 with one goal in mind:</p>
-        <p><i>Developing reliable, high quality solutions for challenges present in the Digital Age.</i></p>
-        <p>Dev Null Productions has a solid track record of rapidly delivering production ready technologies in many domains.</p>
-        <p>Why take a risk with an unknown firm or a low-budget solution for your mission critical application?</p>
-        <p>Schedule a <a href="mailto:devnullproductions@gmail.com">consultation</a> today!</p>
+      <div id="cards">
+        <div class="left-card">
+          <div>
+            <img src="./assets/imgs/Innvoation.svg" />
+            <h5 class="title">Innovation.</h5>
+            <p class="content">We developed first to market patented Blockchain tracker.</p>
+          </div>
+          <div>
+            <img src="./assets/imgs/reliable.svg" />
+            <h5 class="title">Reliable & Excellence.</h5>
+            <p class="content">With the solid track record, we deliver solutions rapidly for clients in various domain.</p>
+          </div>
+        </div>
+        <div class="right-card">
+          <div class="right-card-wrapper">
+            <h5 class="title">Proudly involved with core Blockchain developement.</h5>
+            <a href="#">Read Articles</a>
+          </div>
+        </div>
+      </div>
+      <div id="mainContent">
+        <div class="section1">
+          <div class="section-wrapper">
+            <h4 class="section-header">
+              Design for convienient, built for scalibility. 
+            </h4>
+            <p class="section-header-description">
+              Over the year, we hear the user desires. With multiple of cycle of developement,  we are proundly be the product leader for Blockchain tracker.
+            </p>
+            <div class="image-card d-flex">
+              <div class="image-card__image">
+                <img src="./assets/imgs/img-homepage-p-btt.svg" />
+              </div>
+              <div class='image-card__content'>
+                <h5>BT</h5>
+                <h4>Blockchain Transaction Tracker</h4>
+                <p>Our transaction tracking suite (patent pending) is the perfect solution to receive email, text message, and webhook alerts on Blockchain activity with no fuss.</p>
+              </div>
+            </div>
+            <div class="image-card image-card-2 d-flex flex-row-reverse">
+              <div class="image-card__image">
+                <img src="./assets/imgs/img-homepage-p-dei.svg" />
+              </div>
+              <div class='image-card__content'>
+                <h5>XRP INTEL</h5>
+                <h4>Decentralized Exchange Intelligence</h4>
+                <p>Our DEX Intelligence suite analyzes several major markets and delivers consistent, reliable stats.</p>
+              </div>
+            </div>
+            <div class="text-center">
+              <b-button variant="primary" class="text-white px-5 py-3">Lear more about our products</b-button>
+            </div>
+          </div>
+        </div>
+        <div class="section2 carousel-section">
+          <div class="section-wrapper">
+            <h4 class="section-header our-partner">
+              Our partners
+            </h4>
+            <CustomCarousel />
+          </div>
+        </div>
       </div>
     </div>
 
-    <div id="promo">
+    <!-- <div id="promo">
       <iframe :width="mq_xs ? 280 : 560"
               :height="mq_xs ? 158 : 315"
               src="https://www.youtube.com/embed/THZZd3693AM"
@@ -22,7 +78,7 @@
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowfullscreen>
       </iframe>
-    </div>
+    </div> -->
 
     <!--<div id="partners_wrapper">
       <div id="partners_title_wrapper">
@@ -44,32 +100,249 @@
 
 <script>
 import MainLayout from './components/MainLayout'
-
+import CustomCarousel from '@/components/CustomCarousel'
 export default {
   name: 'Landing',
   components: {
-    MainLayout
+    MainLayout,
+    CustomCarousel
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import './scss/custom.scss';
 #landing{
   width: 100%;
 }
 
 #title_wrapper{
   width: 100%;
-  min-height: 500px;
-  padding-top: 150px;
-  background-image: url("./assets/blue-bg.png");
-  background-size: 100% 100%;
+  min-height: 904px;
+  padding-top: 34vh;
+  background-image: url("./assets/imgs/home-header-bg.png");
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
 }
 
 #title_wrapper h1{
   text-align: center;
-  color: white;
-  font-family: Lora;
+  color: $white;
+  font-family: DM Sans;
+  font-weight: bold;
+  font-size: 72px;
+  line-height: 80px;
+  max-width: 864px;
+}
+
+#cards {
+  max-width: 1314px;
+  margin: 0 auto;
+  position: relative;
+}
+
+#cards .left-card {
+  background-color: $primary-blue;
+  padding: 61px 114px;
+  position: absolute;
+  display: flex;
+  max-width: 866px;
+  z-index: 1;
+  bottom: -178px;
+}
+
+#cards .left-card > div:first-child {
+  margin-right: 32px;
+}
+
+#cards .left-card img {
+  margin-bottom: 16px;
+}
+
+#cards .left-card .title,
+#cards .right-card .title {
+  font-family: DM Sans;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  color: $white;
+}
+
+#cards .left-card .content {
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  color: $white;
+  margin-bottom: 0;
+}
+
+#cards .right-card {
+  background: #2F7BBD;
+  max-width: 448px;
+  position: absolute;
+  right: 0;
+  bottom: -212px;
+  z-index: 1;
+}
+
+#cards .right-card-wrapper {
+  padding: 111px 24px 65px 32px;
+  position: relative;
+}
+
+#cards .right-card-wrapper:before {
+  position: absolute;
+  left: -32px;
+  bottom: 0;
+  content: '';
+  width: 0;
+  height: 0;
+  border-top: solid 34px #235C8E;
+  border-left: solid 32px transparent;
+}
+
+#cards .right-card .title {
+  margin-bottom: 44px;
+}
+
+#cards .right-card a {
+  font-family: DM Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 16px;
+  color: $white;
+  display: flex;
+  align-content: center;
+  text-decoration: none;
+}
+
+#cards .right-card a:after {
+  margin-left: 10px;
+  content: url("./assets/imgs/arrow-right.svg");
+  width: 16px;
+  height: 16px;
+}
+
+#mainContent {
+  position: relative;
+}
+
+#mainContent .section1 {
+  background: linear-gradient(121.51deg, #172837 13.32%, #3377B3 87.49%);
+  padding-top: 340px;
+}
+
+#mainContent .section1 .section-wrapper {
+  max-width: 1188px;
+  margin: 0 auto;
+}
+
+.section-header {
+  font-family: DM Sans;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 34px;
+  line-height: 40px;
+  color: $primary-blue;
+  text-align: center;
+  letter-spacing: 0.5px;
+}
+
+.section-header-description {
+  max-width: 640px;
+  margin: 0 auto;
+  margin-top: 12px;
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  text-align: center;
+  color: $grey-0;
+}
+
+.image-card {
+  color: $white;
+  margin-top: 50px;
+}
+
+.image-card__content {
+  margin-top: 50px;
+  padding-right: 50px;
+  padding-left: 0;
+}
+
+.image-card:last-child {
+  margin-top: 80px;
+  margin-bottom: 60px;
+}
+
+.image-card-2 .image-card__content {
+  padding-left: 50px;
+  padding-right: 0;
+}
+
+.image-card__content h5 {
+  width: 104px;
+  height: 104px;
+  padding: 20px;
+  text-align: center;
+  background: #1C84DE;
+  border-radius: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 24px;
+}
+
+.image-card__content h4 {
+  font-family: DM Sans;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 34px;
+  line-height: 40px;
+  letter-spacing: 0.5px;
+  color: $white;
+  margin-bottom: 20px;
+}
+
+.image-card__content p {
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  color: #CCCDCE;
+}
+
+.section1 button {
+  font-family: DM Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 16px;
+  text-transform: uppercase;
+  margin-bottom: 102px;
+}
+
+.section2 {
+  background-color: $white;
+  padding: 56px;
+}
+
+.section2 .section-wrapper {
+  max-width: 1108px;
+  margin: 0 auto;
+}
+
+.our-partner {
+  margin-bottom: 56px;
 }
 
 #content{
---
+++ src/components/CustomCarousel.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="position-relative px-5">
+    <VueSlickCarousel v-bind="settings" ref="carousel">
+      <div
+        class="carousel-item"
+        v-for="(item, index) in arr"
+        :key="index"
+      >
+        <div>
+        </div>
+      </div>
+    </VueSlickCarousel>
+    <div>
+      <img class="slick-icon slick-prev-icon" src="../assets/imgs/arrow_backward.svg" @click="showNext" />
+      <img class="slick-icon slick-next-icon" src="../assets/imgs/arrow_forward.svg" @click="showNext" />
+    </div>
+  </div>
+</template>
+ 
+<script>
+  import VueSlickCarousel from 'vue-slick-carousel'
+  import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+  // optional style for arrows & dots
+  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+ 
+  export default {
+    name: 'CustomCarousel',
+    components: { VueSlickCarousel },
+    data() {
+      return {
+        settings: {
+          "dots": false,
+          "focusOnSelect": true,
+          "infinite": true,
+          "speed": 500,
+          "slidesToShow": 4,
+          "slidesToScroll": 2,
+          "touchThreshold": 5
+        },
+        arr: [1, 2, 3, 4, 5, 6]
+      }
+    },
+    methods: {
+      showNext() {
+        this.$refs.carousel.next()
+      },
+    },
+  }
+</script> 
+<style scoped lang="scss">
+@import '@/scss/custom.scss';
+
+.carousel-item div {
+  background: $grey-1;
+  width: 191px;
+  height: 191px;
+  margin: 0 auto;
+}
+
+.slick-icon {
+  cursor: pointer;
+}
+
+.slick-next-icon {
+  position: absolute;
+  top: calc(50% - 15px);
+  right: 0;
+}
+
+.slick-prev-icon {
+  position: absolute;
+  top: calc(50% - 15px);
+  left: 0;
+}
+</style>
