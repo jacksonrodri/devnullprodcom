f9268505220cf990592e4f19ad2237ac4aceb4a1
jacksonrodri <jacksonrodriguez329@gmail.com>
2021-05-05 08:57:06 -0400

    Replace data in articleDetail page

--- src/mixins/HasPosts.js
+++ src/mixins/HasPosts.js
@@ -19,6 +19,12 @@ export default {
       }.bind(this)).filter((_, index) => index < this.count)
     },
 
+    recent_enabled_posts : function(){
+      return this.enabled_posts.slice().sort(function(p1, p2){
+        return this.parsed_dates[p2.path] - this.parsed_dates[p1.path];
+      }.bind(this)).filter((_, index) => index < 3)
+    },
+
     parsed_dates : function(){
       return Object.fromEntries(posts.map(function(post){
         return [post.path, new Date(Date.parse(post.date))];
--- src/pages/article/ArticleDetail.vue
+++ src/pages/article/ArticleDetail.vue
@@ -9,18 +9,14 @@
           >
             Back to Article
           </a>
-          <h3 class="typography-h3 mb-3">XRP vs XLM - A Technical Comparison - Part 4</h3>
+          <h3 class="typography-h3 mb-3">{{ post.title }}</h3>
           <div class="article-item-detail">
-            <p class="article-item-detail-date body-2 mb-3">March 4th</p>
+            <p class="article-item-detail-date body-2 mb-3">{{parsed_dates[post.path].toLocaleString('default', { month: 'long' })}} {{parsed_dates[post.path].getDate()}}</p>
             <div class="article-item-detail-image mb-3">
-              <img src="@/assets/imgs/img-homepage-p-dei.svg">
+              <img :src="post_img(post)">
             </div>
             <p class="article-item-detail-content body-1 mb-4 pb-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec, diam elementum nec ornare amet. Viverra dolor pellentesque pellentesque sed eu. Ut felis tristique in amet, vitae fringilla. Nisi mauris hac volutpat viverra ultricies aliquet rhoncus. Elementum felis, et placerat integer at viverra. Euismod mauris hendrerit ipsum ultrices imperdiet varius. Sed viverra tristique massa sed nibh in. Dui tempus aliquet sem est ut. Nulla scelerisque non varius sit amet praesent pulvinar urna.
-              Tristique cras viverra leo hac. Mauris, nisl cursus proin ornare hac. Diam non justo diam rutrum pellentesque consequat, euismod volutpat nisl. Cursus aliquam turpis dictumst sit varius convallis arcu. Metus, in donec mattis fermentum pretium. Sit enim, tortor pellentesque purus. Lectus in mauris sit luctus pulvinar ac et. Pulvinar fringilla cras gravida ultricies bibendum nullam nisl lacus. Facilisis fames nec ac tristique id. Non orci pretium justo, dolor at sollicitudin amet quisque. Viverra consectetur eget commodo tortor ultricies et, in dictum aliquet. Commodo enim, morbi ac, sit. A netus cursus in leo lacus, dui ultricies placerat sit. Cursus quam vel, donec id interdum nibh sit. Sit purus commodo, arcu nibh ultrices.
-              Porta sed in eu, nulla commodo volutpat condimentum. Enim feugiat erat eu non, risus in bibendum blandit. Eget risus hac felis nisl. Vitae id metus vitae nibh eu feugiat egestas nulla. Aliquam vitae aenean proin est at nunc, vivamus sed. Eu semper varius ut dictum. A, quis nisl turpis sit nibh tempus. Est et diam morbi mus viverra at faucibus amet. Ipsum sit viverra aliquet nam volutpat ridiculus nullam donec. Ultrices tellus rhoncus ornare urna posuere lobortis. Cursus lorem nunc scelerisque volutpat eleifend. Et nisi, nulla in ornare.
-              A nulla et, nullam pellentesque. Duis fringilla magna et gravida. Ac etiam nec porttitor pulvinar pretium quis. Ornare sed vulputate tempor augue massa. Est augue facilisi pulvinar luctus vitae purus purus pellentesque. Tempus, quis gravida amet morbi adipiscing lobortis. Sollicitudin amet aliquam dignissim vestibulum. Maecenas erat molestie et nam pharetra sit.
-              Tellus pulvinar vulputate morbi amet at at blandit. Malesuada magna sit duis tristique eu, habitant imperdiet et. Volutpat, leo faucibus nibh massa nisi lectus mi et. Nunc etiam volutpat ullamcorper ullamcorper sapien sagittis bibendum. Nunc consectetur eget facilisis nullam. Id tortor a id mattis porttitor quis iaculis. Diam, ultrices odio egestas mauris commodo eget velit. Dictum augue faucibus facilisis sollicitudin justo, in.
+              {{ post.summary }}
             </p>
           </div>
           <div class="article-item-share">
@@ -29,10 +25,9 @@
           <div class="article-section">
             <div class="article-list">
               <ArticleItem
-                v-for="(item, index) of getArticles"
-                :key="index"
+                v-for="(item) of recent_enabled_posts"
+                :key="item.path"
                 :item="item"
-                :id="index"
               />
             </div>
           </div>
@@ -46,6 +41,8 @@
 import MainLayout from "@/components/layout/MainLayout"
 import ArticleItem from "@/components/article/ArticleItem"
 import SocialShare from "@/components/share/SocialShare"
+import HasPosts from "@/mixins/HasPosts"
+import Posts from "@/assets/posts"
 
 export default {
   name: "ArticleDetail",
@@ -54,43 +51,18 @@ export default {
     ArticleItem,
     SocialShare
   },
-  data() {
-    return {
-      articles: [
-        {
-          date: "March 4th",
-          subject: "XRP vs XLM - A Technical Comparison - Part 4",
-          tags: [
-            "xrp",
-            "xlm",
-            "technical"
-          ]
-        },
-        {
-          date: "March 4th",
-          subject: "XRP vs XLM - A Technical Comparison - Part 4",
-          tags: [
-            "xrp",
-            "xlm",
-            "technical"
-          ]
-        },
-        {
-          date: "March 4th",
-          subject: "XRP vs XLM - A Technical Comparison - Part 4",
-          tags: [
-            "xrp",
-            "xlm",
-            "technical"
-          ]
-        },
-      ],
-    }
-  },
+  mixins: [
+    HasPosts
+  ],
   computed: {
-    getArticles: function() {
-      return this.articles
-    }
+    post: function(){
+      var path = this.$route.path.split("/")
+          path = path[path.length-1]
+
+      return Posts.find(function(post){
+        return post.path == path;
+      })
+    },
   },
   methods: {
     goBackArticle: function () {
